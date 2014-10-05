72231715e3732ddf792eab73fcb42138ebcebe54
Benji Dalton <daylightsavings@gmail.com>
2014-10-05 12:16:18 +0000

    Created controller with index and about views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bloccit::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
