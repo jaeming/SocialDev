125b2686376bb708efaf1a02fa16233745af30fd
Benji Dalton <daylightsavings@gmail.com>
2014-10-10 09:47:13 +0000

    styled fonts, adjusted margins

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,17 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
   def edit
   end
 end
