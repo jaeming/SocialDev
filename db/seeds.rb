6df6db4289d48c87bd890345931a4241a5731b89
Benji Dalton <daylightsavings@gmail.com>
2014-10-13 12:43:37 +0000

    associating with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,42 @@
 require 'faker'
- 
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+
+# Create Users
+5.times do
+user = User.new(
+  name:      Faker::Name.name,
+  email:     Faker::Internet.email,
+  password:  Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Create Posts
+50.times do
+ Post.create!(
+   title:  Faker::Lorem.sentence,
+   body:   Faker::Lorem.paragraph,
+   user: users.sample
+ )
+end
+posts = Post.all
+
+
+# Create Comments
+100.times do
+ Comment.create!(
+   post: posts.sample,
+   body: Faker::Lorem.paragraph
+ )
+end
+
+User.first.update_attributes!(
+  email: 'daylightsavings@gmail.com',
+  password: 'helloworld',
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
