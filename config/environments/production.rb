679f75b3882c7a75a0b696f36f052d792f75e9d7
Benji Dalton <daylightsavings@gmail.com>
2014-10-17 13:42:10 +0000

    updated production env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,6 @@ Bloccit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = [ host: 'snowful.heroku.com']
+  # http://bloccit.herokuapp.com
+   config.action_mailer.default_url_options = { host: 'snowful.herokuapp.com' }
 end
