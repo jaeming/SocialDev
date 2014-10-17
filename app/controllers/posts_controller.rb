2ffbb1f091bbf4cb444297bd8f02e3f681c08ff4
Benji Dalton <daylightsavings@gmail.com>
2014-10-17 13:19:36 +0000

    finished topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,24 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
     @post = Post.new
+    @topic = Topic.find(params[:topic_id])
     authorize @post
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-     authorize @post
+    @post.topic = @topic
+    authorize @post
+
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -22,16 +27,18 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
      authorize @post
   end
 
   def update
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
      authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error in saving the post. Try again."
       render :edit
