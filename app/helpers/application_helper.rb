bbee0ac49558e1d3f422899cea5079c4336e4e84
Benji Dalton <daylightsavings@gmail.com>
2014-10-19 12:52:52 +0000

    exploring moving markdown from helper to model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-
-  def markdown(text)
-    sanitize Kramdown::Document.new(text).to_html
-  end
-
 end
 
 def my_name
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,15 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+
+  def markdown_body
+    render_as_markdown body
+  end
+
+  private
+
+  def render_as_markdown(text)
+    Kramdown::Document.new(text).to_html
+  end
 end
