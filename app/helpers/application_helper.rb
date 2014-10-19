d465dd5031650dbd4f4cc650174c91c82e8cf688
Benji Dalton <daylightsavings@gmail.com>
2014-10-19 09:02:45 +0000

    integrated redcarpet markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
 end
 
+def markdown(text)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render text).html_safe
+end
+
 def my_name
   p "Benji Dalton"
 end
