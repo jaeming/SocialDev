81fc5196b702665795c050e9bff7972ffc692d47
Benji Dalton <daylightsavings@gmail.com>
2014-10-21 14:33:13 +0000

    finished infinity scrolling with non-js pagination fallback!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,15 @@ $(function() {
   $(document).foundation();
 });
 
-//= require scrollinity
+$(document).ready(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 70) {
+        $('.pagination').text("Please Wait...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
