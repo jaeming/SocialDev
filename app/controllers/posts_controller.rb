fc43f915e894445a14802c881d9287fcfdc7c79a
jaeming <daylightsavings@gmail.com>
2016-02-01 18:21:50 -0600

    clean up indentation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
-def index
-  @posts = Post.visible_to(current_user).where("posts.created_at > ?", 500.days.ago).paginate(page: params[:page], per_page: 10)
-end
+  def index
+    @posts = Post.visible_to(current_user)
+                 .where("posts.created_at > ?", 500.days.ago)
+                 .paginate(page: params[:page], per_page: 10)
+  end
 
 end
