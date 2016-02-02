169051eeb48e04becaba927bc1b6d458c9654895
jaeming <daylightsavings@gmail.com>
2016-02-01 18:16:07 -0600

    cleans up indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-      rescue_from Pundit::NotAuthorizedError do |exception|
-        redirect_to root_url, alert: exception.message
-      end
+    rescue_from Pundit::NotAuthorizedError do |exception|
+      redirect_to root_url, alert: exception.message
+    end
 
-      def after_sign_in_path_for(resource)
-        topics_path
-      end
+    def after_sign_in_path_for(resource)
+      topics_path
+    end
 
    protected
 
