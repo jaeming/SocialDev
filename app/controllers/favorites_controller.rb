f6f551c36e5e117dae12cc370a320ce91c5eb918
jaeming <daylightsavings@gmail.com>
2016-02-01 18:20:31 -0600

    remove empty lines

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,9 @@
 class FavoritesController < ApplicationController
+  
   def create
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post: @post)
     authorize favorite
-
     if favorite.save
       flash[:notice] = "favorite saved!"
       redirect_to [@post.topic, @post]
@@ -17,7 +17,6 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.find(params[:id])
     authorize favorite
-
     if favorite.destroy
       flash[:notice] = "favorite removed."
       redirect_to [@post.topic, @post]
